50e61aea564c8c00f32bfa749d4c2d8eb2b36b0b
Sameer <87966531+SameerMushtaq@users.noreply.github.com>
2021-11-16 10:32:18 +0530

    Update sparse_matrix_functional.c

--- Arrays/sparse_matrix_functional.c
+++ Arrays/sparse_matrix_functional.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #define ROWS 4
 #define COLS 4
-/*This C program is used to check if a matrix is a sparse Matrix. If the number of zeros in a matrix exceeds (ROWS*COLS)/2, where ROWS, COLS is the dimension of the matrix, matrix is sparse matrix. Sparse matrix has more zero elements than nonzero elements.
- */
-void findSparseMatrix(int matrix[ROWS][COLS]);
 
-void initMatrix(int matrix[ROWS][COLS],
+  void findSparseMatrix(int matrix[ROWS][COLS]);
+
+  void initMatrix(int matrix[ROWS][COLS],
                 int (*init)(int, int)) {
   for(int i = 0; i < ROWS; i++){
     for(int j = 0; j < COLS; j++){
       matrix[i][j] = (*init)(i, j);
     }
   }
-}
+ }
 
 int setZero(int i, int j) {
   return 0;
